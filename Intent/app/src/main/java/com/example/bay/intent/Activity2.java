3bba6633663f36d43ee44f02b1c0dfb19250764e
irvan ariyanto <irvan.ariyanto104@gmail.com>
2016-10-01 08:23:13 +0700

    Add Intent Project

---
+++ Intent/app/src/main/java/com/example/bay/intent/Activity2.java
@@ -0,0 +1,25 @@
+package com.example.bay.intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by Bay on 10/1/2016.
+ */
+public class Activity2 extends Activity {
+    TextView username,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        username=(TextView)findViewById(R.id.username);
+        password=(TextView)findViewById(R.id.password);
+
+        Bundle bundle=getIntent().getExtras();
+        username.setText(bundle.getString("username"));
+        password.setText(bundle.getString("password"));
+    }
+}
---
+++ Intent/app/src/main/java/com/example/bay/intent/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.bay.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editUsername,editPassword;
+    String username,password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editUsername=(EditText)findViewById(R.id.editUser);
+        editPassword=(EditText)findViewById(R.id.editPassword);
+        login=(Button)findViewById(R.id.buttonLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Activity2.class);
+
+                Bundle bundle=new Bundle();
+                username=editUsername.getText().toString();
+                password=editPassword.getText().toString();
+                if(username.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Username belum diisi",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Password belum diisi",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                bundle.putString("username",username);
+                bundle.putString("password",password);
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
