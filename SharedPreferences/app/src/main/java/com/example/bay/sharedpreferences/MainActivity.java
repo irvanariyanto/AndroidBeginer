b0c5134a2bc3801ad568472cbd61130274f00dc7
irvan.ariyanto <irvan.ariyanto104@gmail.com>
2017-02-27 23:57:15 +0700

    Add project SharedPreferences

---
+++ SharedPreferences/app/src/main/java/com/example/bay/sharedpreferences/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bay.sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("BelajarSharedPreferences", Context.MODE_PRIVATE);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("konten", editText.getText().toString());
+                editor.commit();
+            }
+        });
+    }
+}
